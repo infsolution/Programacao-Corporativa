56cd74d3dc498db1a9bf06a2bf95ba16f5e7ca80
infsolution <clsinfsolution@gmail.com>
2017-06-01 11:29:33 -0300

    Conclusão

--- Activity-B/Facebook/src/model/Grupo.java
+++ Activity-B/Facebook/src/model/Grupo.java
@@ -1,5 +1,46 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Grupo {
+	private long id;
+	private String name;
+	private String description;
+	private long[] idusers;
+	private ArrayList<Post> posts = new ArrayList<>();
+	public Grupo(String name){
+		this.name=name;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public long[] getIdusers() {
+		return idusers;
+	}
+	public void setIdusers(long[] idusers) {
+		this.idusers = idusers;
+	}
+	public ArrayList<Post> getPosts() {
+		return posts;
+	}
+	public void setPosts(ArrayList<Post> posts) {
+		this.posts = posts;
+	}
+	
 
 }
--- Activity-B/Facebook/src/model/User.java
+++ Activity-B/Facebook/src/model/User.java
@@ -1,5 +1,58 @@
 package model;
 
-public class User {
+import java.util.ArrayList;
 
+public class User {
+	private long id;
+	private String name;
+	private String datBirth;
+	private long[] idsUser;
+	private long[] idsFanpage;
+	private long[] idsGrupos;
+	public User(String name){
+		this.name = name;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDatBirth() {
+		return datBirth;
+	}
+	public void setDatBirth(String datBirth) {
+		this.datBirth = datBirth;
+	}
+	public long[] getIdsUser() {
+		return idsUser;
+	}
+	public void setIdsUser(long[] idsUser) {
+		this.idsUser = idsUser;
+	}
+	public long[] getIdsFanpage() {
+		return idsFanpage;
+	}
+	public void setIdsFanpage(long[] idsFanpage) {
+		this.idsFanpage = idsFanpage;
+	}
+	public long[] getIdsGrupos() {
+		return idsGrupos;
+	}
+	public void setIdsGrupos(long[] idsGrupos) {
+		this.idsGrupos = idsGrupos;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+	
 }
+
